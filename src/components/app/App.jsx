4999efba1e290bd6c8dd1d94a494db8b86d850c4
ivnlsv <ivan.losev@denuo.legal>
2025-02-04 16:34:53 +0300

    upd

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,13 @@
 import {Header} from '../header/header';
 import {Footer} from '../footer/footer';
 import { Layout } from '../layout/layout';
-
 //import {Main} from '../main/main';
 
 function App() {
   return (
     <Layout>
       <Header/>
-        <main></main>
+      <main></main>
       <Footer/>     
     </Layout>
  );
