31d8e3e486c82e0bb6e91f0a33e7cbd5caf107fd
ivnlsv <ivan.losev@denuo.legal>
2025-02-04 17:07:15 +0300

    upd

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Header} from './components/header/header';
+import {Footer} from './components/footer/footer';
+import { Layout } from './components/layout/layout';
+//import {Main} from '../main/main';
+
+function App() {
+  return (
+    <Layout>
+      <Header/>
+      <main></main>
+      <Footer/>     
+    </Layout>
+ );
+}
+
+export default App;
