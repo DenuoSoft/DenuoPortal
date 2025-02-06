f4ac26bf96b87e178e5a11ab32f1895a31ecf6e8
ivnlsv <ivan.losev@denuo.legal>
2025-02-06 18:37:29 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import {Header} from './components/header/header';
-import {Footer} from './components/footer/footer';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 import { Layout } from './components/layout/layout';
-import { Main } from './components/main/main'
-import { HR } from "./components/main/hr/hr";
-import { Marketing } from "./components/main/marketing/marketing";
-import { IT } from "./components/main/it/it";
-import { Form } from "./components/main/form/form";
-import { Phonebook } from "./components/main/phonebook/phonebook";
-import { Routes, Route } from "react-router-dom"
+import { Main } from './components/main/main';
+import { HR } from './components/main/hr/hr';
+import { Marketing } from './components/main/marketing/marketing';
+import { IT } from './components/main/it/it';
+import { Form } from './components/main/form/form';
+import { Phonebook } from './components/main/phonebook/phonebook';
+import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
-  return (
-    <Layout>
-      <Header />
-        
-      <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/hr" element={<HR />} />
-            <Route path="/marketing" element={<Marketing />} />
-            <Route path="/it" element={<IT />} />
-            <Route path="/form" element={<Form />} />
-            <Route path="/phonebook" element={<Phonebook />} />
-          </Routes>
-      
-      <Footer/>     
-    </Layout>
- );
-}
-
+	return (
+		<Layout>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/hr' element={<HR />} />
+				<Route path='/marketing' element={<Marketing />} />
+				<Route path='/it' element={<IT />} />
+				<Route path='/form' element={<Form />} />
+				<Route path='/phonebook' element={<Phonebook />} />
+			</Routes>
+			<Footer />
+		</Layout>
+	);
+};
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,14 +1,7 @@
 //import css from './main.module.scss';
-import { Tabs } from "../tabs/tabs";
+import { Tabs } from '../tabs/tabs';
 import { tabs, content } from './main-data';
 //import { Outlet} from "react-router-dom";
 export const Main = () => {
-
-	return 	<>
-	<Tabs tabs={tabs} content={content} />	
-		
-		
-	</>
-	
-	
+	return <Tabs tabs={tabs} content={content} />;
 };
