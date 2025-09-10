c6c37c77b2ce390aa700781a37a0c379ecc96839
ivnlsv <ivan.losev@denuo.legal>
2025-09-10 18:11:29 +0300

    Some changes in min.jsx
    added to BrowserRouter future={{
    					v7_relativeSplatPath: true,
    					v7_startTransition: true,
    				}} due to console message React Router Future Flag Warning: Relative route resolution within Splat routes is changing in v7. You can use the `v7_relativeSplatPath` future flag to opt-in early

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,12 @@ import store from './store';
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
+			<BrowserRouter
+				future={{
+					v7_relativeSplatPath: true,
+					v7_startTransition: true,
+				}}
+			>
 				<App />
 			</BrowserRouter>
 		</Provider>
